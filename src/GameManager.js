b64ef8c871c23a1db36e14e70c4559a677672118
nlom0218 <nlom0218@naver.com>
2022-12-12 20:09:16 +0900

    feat: 다리의 길이 입력 받기

--- src/GameManager.js
+++ src/GameManager.js
@@ -3,6 +3,16 @@ const { InputView, OutputView } = require('./View/IOView');
 class GameManager {
   start() {
     OutputView.printStartMessage();
+
+    this.requestBridgeSize();
+  }
+
+  requestBridgeSize() {
+    InputView.readBridgeSize((bridegSize) => this.handleBridgeSize(bridegSize));
+  }
+
+  handleBridgeSize(bridegSize) {
+    console.log(`다리의 길이는 ${bridegSize}입니다.`);
   }
 }
 
--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,11 +1,17 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 const InputView = {
+  REQUEST_BRIDGE_SIZE: '\n다리의 길이를 입력해주세요.\n',
+
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callBack) {
+    Console.readLine(this.REQUEST_BRIDGE_SIZE, callBack);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
