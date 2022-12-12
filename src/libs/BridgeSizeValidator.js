949dc5919785f30bd74b9382fd1afd69f07031f9
nlom0218 <nlom0218@naver.com>
2022-12-12 22:51:48 +0900

    refactor: BridgeSizeValidator 에러 메시지 상수화하기

--- src/libs/BridgeSizeValidator.js
+++ src/libs/BridgeSizeValidator.js
@@ -1,4 +1,7 @@
 const BridgeSizeValidator = {
+  NUMBER_ERROR: '[ERROR] 숫자를 입력해주세요.',
+  SCOPE_ERROR: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
+
   validation(bridgeSize) {
     this.isNumber(bridgeSize);
     this.isCorrectScope(bridgeSize);
@@ -6,13 +9,11 @@ const BridgeSizeValidator = {
 
   isNumber(bridgeSize) {
     const numberRegExp = /^[0-9]+$/;
-    if (!numberRegExp.test(bridgeSize))
-      throw new Error('[ERROR] 숫자를 입력해주세요.');
+    if (!numberRegExp.test(bridgeSize)) throw new Error(this.NUMBER_ERROR);
   },
 
   isCorrectScope(bridgeSize) {
-    if (bridgeSize < 3 || bridgeSize > 20)
-      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    if (bridgeSize < 3 || bridgeSize > 20) throw new Error(this.SCOPE_ERROR);
   },
 };
 
